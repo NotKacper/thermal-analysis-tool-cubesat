0770c2d9ee9e1fc4be00aece2da88f998ccf4128
Kacper Piotr Lubczynski <kacperlubczynski564@gmail.com>
2023-11-07 20:20:23 +0000

    Finalised two independent variable model

--- Matrices/TemperatureMatrix.py
+++ Matrices/TemperatureMatrix.py
@@ -1,6 +1,7 @@
 class TemperatureMatrix:
-    def __init__(self, initial_temperature : float):
-        self.matrix = [[initial_temperature, initial_temperature] for i in range(3)]
+    def __init__(self, initial_temperature: float):
+        self.matrix = [[initial_temperature, initial_temperature]
+                       for i in range(3)]
 
     def update_matrix(self, mass: float, specific_heat_capacity: float, heat_flow_matrix: list[list[float]], delta_time: float) -> None:
         for i in range(len(self.matrix)):
